36347a3c0081f69dd0c8afd932974eaa07b38cd0
CHutchnson <cheyennehutchinson@outlook.com>
2022-04-25 20:56:24 +0100

    added methods to service class

--- src/main/java/com/qa/playerbase/service/PlayerService.java
+++ src/main/java/com/qa/playerbase/service/PlayerService.java
@@ -7,5 +7,35 @@ import com.qa.playerbase.player.*;
 
 @Service
 public class PlayerService {
+	
+	private List<Player> players = new ArrayList<>();
+
+    public Player addlayer(Player player) {
+        // Add new Person
+        this.players.add(player);
+        // Return last added Person from List
+        return this.players.get(this.players.size() - 1);
+    }
+
+    public List<Player> getAllPlayer() {
+        // Return the whole List
+        return this.players;
+    }
+
+    public Player updatePlayer(int id, Player player) {
+        // Remove existing Person with matching 'id'
+        this.players.remove(id);
+        // Add new Person in its place
+        this.players.add(id, player);
+        // Return updated Person from List
+        return this.players.get(id);
+    }
+
+    public Player removePlayer(int id) {
+        // Remove Person and return it
+        return this.players.remove(id);
+    }
 
 }
+
+

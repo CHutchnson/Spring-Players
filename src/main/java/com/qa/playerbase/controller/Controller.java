06581afc3f9836bec0f7bf560a36946c535b46f9
CHutchnson <cheyennehutchinson@outlook.com>
2022-04-25 20:29:52 +0100

    added CRUD methods to the controller

--- src/main/java/com/qa/playerbase/controller/Controller.java
+++ src/main/java/com/qa/playerbase/controller/Controller.java
@@ -9,7 +9,6 @@ import java.util.ArrayList;
 import java.util.List;
 import com.qa.playerbase.player.*;
 
-import javax.websocket.server.PathParam;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -24,5 +23,31 @@ import org.springframework.web.bind.annotation.PathVariable;
 public class Controller {
 	
 	List<Player> players = new ArrayList<>();
-
+	
+	 @PostMapping("/create")
+	    public boolean addPlayers(@RequestBody Player player) {
+	        return this.players.add(player);
+	    }
+	
+	 @GetMapping("/getAll")
+	    public List<Player> getAllPlayers() {
+			return this.players;
+	        //
+	    }
+	 
+	 @PutMapping("/update/{id}")
+	    public Player updatePerson(@PathVariable int id, @RequestBody Player player) {
+	        // Remove existing Person with matching 'id'
+	        this.players.remove(id);
+	        // Add new Person in its place
+	        this.players.add(id, player);
+	        return this.players.get(id);
+	    }
+	 
+	 @DeleteMapping("/delete/{id}")
+	    public Player removePerson(@PathVariable int id) {
+	        // Remove Person and return it
+	        return this.players.remove(id);
+	    }
+	 
 }

8679c3619e28417cbcc7ca291724d4f5844faede
CHutchnson <cheyennehutchinson@outlook.com>
2022-04-25 20:44:14 +0100

    created service class

---
+++ src/main/java/com/qa/playerbase/service/PlayerService.java
@@ -0,0 +1,11 @@
+package com.qa.playerbase.service;
+
+import org.springframework.stereotype.Service;
+import java.util.ArrayList;
+import java.util.List;
+import com.qa.playerbase.player.*;
+
+@Service
+public class PlayerService {
+
+}

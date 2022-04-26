380b6ece6cc0ad1c06f05e2bdfc0ecc34dd47a72
CHutchnson <cheyennehutchinson@outlook.com>
2022-04-26 09:53:36 +0100

    added response status to the custom exception

--- src/main/java/com/qa/playerbase/exception/PlayerNotFoundException.java
+++ src/main/java/com/qa/playerbase/exception/PlayerNotFoundException.java
@@ -5,6 +5,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 
+@ResponseStatus(code = HttpStatus.NOT_FOUND, reason = "Player does not exist")
 public class PlayerNotFoundException extends EntityNotFoundException {
 
 }
